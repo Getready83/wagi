8b1285963825b65f4d1f62e0c276fb576fe4cc8a
unknown <p.getler@o2.pl>
2021-12-20 22:44:04 +0100

    poczatki pracy nad pakowaczem elementów

---
+++ parcel_loader.py
@@ -0,0 +1,33 @@
+# maksymalna waga paczki 20 kg
+# każdy element dodawany może ważyć od 1kg do 10kg
+# Jeżeli kolejny element przekroczy wagę 20kg paczka powinna zostać wysłana
+# a element dodany do kolejnej paczki
+# wszystkie elementy powinny zostać wysłane
+# program powinien pobierać ilość elementów do wysłania
+# podanie elementu o wadze zero kończy działenie programu tak jakby maksymalna
+# liczba paczek została osiągnięta
+# na koniec podsumowanie
+# 1
+# 2
+# 3
+# 4
+
+# program powinien kończyć się błędem gdy element jest większy niż 10kg
+# i mniejszy niż 1 kg ale nie jest dokładnie 0
+ilość_elementów = 0
+print("Witamy w uniwersalnym zautomatyzowanym pakowaczu")
+for nr in range (ilość_elementów):
+    print("podaj ile elementów chcesz zapakować: ")
+    ilość_elementów = int(input())
+    print("podaj wagę: ")
+    waga = float (input())
+
+
+
+
+
+
+print("Podaj wage każdego elementu")
+waga_elementu = float(input())
+
+
