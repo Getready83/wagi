fe5a6d4cc863cebb5d8ee15a97056e6f34df5588
unknown <p.getler@o2.pl>
2021-12-24 14:05:14 +0100

    program parcel_loader działa zgodnie z załżeniami

--- parcel_loader2.py
+++ parcel_loader2.py
@@ -1,31 +1,32 @@
 import sys
 
-
 waga_paczki = 0
 suma_kilogramow = 0
 paczki_zamkniete = 0
 puste_kilogramy = 0
 max_pustych = 0
-numer_paczki_pustych = 0
-
+nr_paczki_zamknietej = 0
+suma_pustych_kilogramow = 0
+blad = False
 print("Witamy w uniwersalnym zautomatyzowanym pakowaczu")
 for nr in range(int(sys.argv[1])):
 
     print("podaj wagę: ")
     waga_elementu = float(input())
     suma_kilogramow = suma_kilogramow + waga_elementu
-#    waga_paczki = waga_paczki + waga_elementu
-#    print("całkowita waga elementow :", suma_kilogramow)
+
     if waga_elementu == 0:
         print("Koniec wysłania")
         break
     if waga_elementu < 1:
+        blad = True
         print("Blad !!! niewłaściwa waga przedmiotu, prosze podac wage "
-              "w przedziale od 1 kg do 10 kg")
+              "w przedziale od 1 kg do 10 kg\n Zacznij od początku !!!")
         break
     if waga_elementu > 10:
+        blad = True
         print("Blad !!! niewłaściwa waga przedmiotu, prosze podac wage "
-              "w przedziale od 1 kg do 10 kg")
+              "w przedziale od 1 kg do 10 kg\n Zacznij od początku !!!")
         break
 
     if waga_paczki + waga_elementu <= 20:
@@ -37,29 +38,19 @@ for nr in range(int(sys.argv[1])):
         print("nr paczki", paczki_zamkniete)
         print("waga:", waga_paczki)
         if puste_kilogramy > max_pustych:
-            numer_paczki_pustych = paczki_zamkniete
+            nr_paczki_zamknietej = paczki_zamkniete
             max_pustych = puste_kilogramy
-
-
-
-#   ilosc_pustych_kilogramow =
-#   ilość_paczek = paczka1 + paczka2 + paczka3
-
-
-#    if waga_paczki >= 20:
-#       print("nastepna paczka")
-
-
-
-
 
 if waga_paczki > 0:
     paczki_zamkniete += 1
     puste_kilogramy = 20 - waga_paczki
+    suma_pustych_kilogramow = paczki_zamkniete * 20 - suma_kilogramow
     if puste_kilogramy > max_pustych:
-        numer_paczki_pustych = paczki_zamkniete
+        nr_paczki_zamknietej = paczki_zamkniete
         max_pustych = puste_kilogramy
-print(f"liczba paczek wysłanych {paczki_zamkniete} szt")
-print(f"Suma kilogramów wysłanych {suma_kilogramow} kg")
-print (f"liczba pustych kilogramów {puste_kilogramy} kg")
-print(f" która paczka miała najwiecej pustych kilogramów {numer_paczki_pustych}!!!")
+
+if not blad:
+    print(f"Ilośc wysłanych paczek {paczki_zamkniete} szt")
+    print(f"Waga wysłanych paczek {suma_kilogramow} kg")
+    print(f"Pustych kilogramów: {suma_pustych_kilogramow} kg")
+    print(f"Paczka nr {nr_paczki_zamknietej} miała najwieciej pustych kg.")
