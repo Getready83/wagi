9a31c8999e5ad09ec7f94cbf2172f1729e285144
unknown <p.getler@o2.pl>
2021-12-29 23:13:13 +0100

    początki gry

---
+++ Kolko_i_Krzyzyk.py
@@ -0,0 +1,32 @@
+""" zaczyna cpu
+# cpu = "X"
+# user = "O"
+# cpu nigdy nie przegra wybiera optymalne ruchy.
+# identyfikator pol
+    1 2 3
+    4 5 6
+    7 8 9
+Przebieg
+cpu wykonuje ruch jeśli wygrał wyswietla komunikat "Przegrana" jeśli ostatnie pole
+zostało wypełnione "Remis"
+
+start
+    - - -
+    - X -
+    - - -
+
+    """
+
+cpu = "X"
+user = "O"
+puste_pole = " -"
+
+
+print("Witam w grze kolko i krzyzyk")
+print("plansza do gry na początku wygląda:\n"
+      " - - -\n"
+      " - - -\n"
+      " - - -\n")
+print("Komputer zaczyna\nPowodzenia")
+
+
