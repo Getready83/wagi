9ba4a89849ea431c644921ef13fda0b3e0a1dbd0
unknown <p.getler@o2.pl>
2022-01-09 22:58:44 +0100

    zalozenia

--- Kolko_i_Krzyzyk.py
+++ Kolko_i_Krzyzyk.py
@@ -19,14 +19,36 @@ start
 
 cpu = "X"
 user = "O"
-puste_pole = " -"
+puste_pole = "-"
 
-
+# drukuje pustą planszę
 print("Witam w grze kolko i krzyzyk")
 print("plansza do gry na początku wygląda:\n"
-      " - - -\n"
-      " - - -\n"
-      " - - -\n")
+      " -  -  - \n"
+      " -  -  - \n"
+      " -  -  - \n")
 print("Komputer zaczyna\nPowodzenia")
+# cpu zaczyna
+
+# okreslenie ruchów dla cpu
+
+# sprawdzenie czy jest wygrana
+
+# drukuj nowa plansze
+
+# kolej user
+
+# wybor pola
+
+# sprawdzenie czy jest wygrana
+
+# jezeli cpu wygral to komentarz
+# jezeli user wygral to komentarz(zalozenie ze cpu nigdy nie przegrywa)
+# jezeli remis to komentarz
 
 
+
+"""
+" {p1}{p2}{p3}\n"
+" {p4}{p5}{p6]\n"
+" {p7}{p8}{p9}\n")"""
--- accountant.py
+++ accountant.py
@@ -2,6 +2,7 @@ import sys
 
 historia = []
 magazyn = {}
+przeglad = []
 saldo = 0
 while True:
     dostep = input("podaj akcję: ")
@@ -18,7 +19,8 @@ while True:
         historia.append("saldo")
         historia.append(kwota)
         historia.append(komentarz)
-    if dostep == "zakup":
+        przeglad == historia
+    if dostep == "zakup":  #and sys.argv[1] == "zakup":
         nazwa = input("Podaj nazwę produktu: ")
         cena = int(input("Podaj cenę: "))
         ilosc = int(input("Podaj ilość:"))
@@ -36,6 +38,7 @@ while True:
         historia.append(nazwa)
         historia.append(cena)
         historia.append(ilosc)
+        przeglad = "zakup", nazwa, cena, ilosc
     if dostep == "sprzedaz":
         nazwa = input("Podaj nazwę produktu: ")
         if nazwa not in magazyn:
@@ -54,31 +57,59 @@ while True:
         historia.append(nazwa)
         historia.append(cena)
         historia.append(ilosc)
-print(historia)
+        if sys.argv[1] == "zakup":
+            nazwa = sys.argv[2]
+            cena = int(sys.argv[3])
+            ilosc = int(sys.argv[4])
+            if saldo - (cena * ilosc) < 0:
+                print("Nie masz wystarczających środków finansowych."
+                      "Twoje saldo to:", saldo)
+                continue
+            if nazwa in magazyn:
+                magazyn[nazwa] += ilosc
+            else:
+                magazyn[nazwa] = ilosc
+            saldo -= cena * ilosc
+            zdarzenie = nazwa, ilosc
+            historia.append("zakup")
+            historia.append(nazwa)
+            historia.append(cena)
+            historia.append(ilosc)
+        if sys.argv[1] == "sprzedaz":
+            nazwa = sys.argv[2]
+            if nazwa not in magazyn:
+                print("Nie ma takiego produktu")
+                continue
+            cena = int(sys.argv[3])
+            ilosc = int(sys.argv[4])
+            if nazwa in magazyn:
+                if magazyn[nazwa] - ilosc >= 0:
+                    magazyn[nazwa] -= ilosc
+                    saldo += cena * ilosc
+                else:
+                    saldo -= cena * ilosc
+                    print("za mała ilosc w magazynie")
+            historia.append("sprzedaz")
+            historia.append(nazwa)
+            historia.append(cena)
+            historia.append(ilosc)
+print(przeglad)
+
+"""print(historia)
 print(magazyn)
-print(saldo)
-if len(sys.argv) < 3:
+print(saldo)"""
+#while True:
+a = len(sys.argv)
+for nr in range(a - 1):
+#if len(sys.argv) < 5:
     if sys.argv[1] == "saldo":
         print(saldo)
-    if sys.argv[1] == "historia":
+    if sys.argv[1] == "historia" or "zakup" or "sprzedaz":
         for i in historia:
             print(i)
     if sys.argv[1] == "magazyn":
         for k, v in magazyn.items():
             print("{}: {}" .format(k, v))
+    if sys.argv[1] == "przeglad":
+        print(przeglad)
 
-
-
-
-"""historia == sys.argv[1]
-for i in range(1,len(historia)):
-    print(historia)
-
-#dostep = sys.argv[1]
-
-log = [historia,saldo,magazyn]
-for nr in sys.argv[1]:
-saldo = sys.argv[1]
-print(sys.argv[1])
-#historia = sys.argv[1]
-"""
