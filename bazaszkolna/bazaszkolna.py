06850045802c5cab6a7354d51a60a079514a1cde
unknown <p.getler@o2.pl>
2022-01-15 23:02:55 +0100

    class stworzone sys.argv niedziała

--- bazaszkolna/bazaszkolna.py
+++ bazaszkolna/bazaszkolna.py
@@ -1,3 +1,35 @@
+import sys
+
+wychowawcy = []
+nauczyciele = []
+uczniowie = []
+grupy = {}
+
+
+class GrupaSzkolna:
+    def __init__(self, numer):
+        self.numer = numer
+        self.wychowawca = None
+        self.nauczyciele = []
+        self.uczniowie = []
+
+    def numer_klasy(self):
+        self.numer = True
+
+    def drukuj(self):
+        print(self.numer)
+        print(self.wychowawca)
+        print(self.nauczyciele)
+        print(self.uczniowie)
+
+
+"""def pobierz_numer_grupy(numer):
+    if numer not in grupy:
+        grupa = GrupaSzkolna(numer)
+        grupy[numer] = grupa
+    return grupy[numer]
+print(grupy)"""
+
 
 class Wychowawca:
     def __init__(self):
@@ -7,13 +39,20 @@ class Wychowawca:
     def wczytanie_wychowawcy(self):
         imie_nazwisko = input()
         klasy = []
+        self.imie_nazwisko = imie_nazwisko
+        self.klasy = klasy
         while True:
             klasa = input()
             if not klasa:
                 break
             klasy.append(klasa)
-        self.klasy = klasy
-        self.imie_nazwisko = imie_nazwisko
+#            grupa = pobierz_numer_grupy(self.klasy)
+#            grupa.wychowawca = self
+
+    def drukuj(self):
+        print(self.imie_nazwisko)
+        print(self.klasy)
+
 
 class Nauczyciel:
     def __init__(self):
@@ -25,14 +64,22 @@ class Nauczyciel:
         imie_nazwisko = input()
         przedmiot = input()
         klasy = []
+        self.imie_nazwisko = imie_nazwisko
+        self.przedmiot = przedmiot
+        self.klasy = klasy
         while True:
             klasa = input()
             if not klasa:
                 break
             klasy.append(klasa)
-        self.imie_nazwisko = imie_nazwisko
-        self.przedmiot = przedmiot
-        self.klasy = klasy
+#            grupa = pobierz_numer_grupy(self.klasy)
+#            grupa.nauczyciel.append(self)
+
+    def drukuj(self):
+        print(self.imie_nazwisko)
+        print(self.przedmiot)
+        print(self.klasy)
+
 
 class Uczen:
     def __init__(self):
@@ -44,12 +91,13 @@ class Uczen:
         klasa = input()
         self.imie_nazwisko = imie_nazwisko
         self.klasa = klasa
+#        grupa = pobierz_numer_grupy(self.klasa)
+#        grupa.uczniowie.append(self)
 
-wychowawcy = []
-nauczyciele = []
-przedmioty = []
-uczniowie = []
-numer_klasy = []
+    def drukuj(self):
+        print(self.imie_nazwisko)
+        print(self.klasa)
+
 
 while True:
     typ_uzytkownika = input()
@@ -67,4 +115,17 @@ while True:
         uczniowie.append(uczen)
     if typ_uzytkownika == "koniec":
         break
+
+
+if sys.argv[1] in wychowawcy:
+    for wychowawca in wychowawcy:
+        wychowawca.drukuj()
+        print(wychowawca)
+"""    
+for nauczyciel in nauczyciele:
+    nauczyciel.drukuj()
+print(nauczyciele)
+for uczen in uczniowie:
+    uczen.drukuj()
 print(uczniowie)
+"""
