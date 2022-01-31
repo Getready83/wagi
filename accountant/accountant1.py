aeb0952449ce03cd2fdc163e5c644e407b503181
unknown <p.getler@o2.pl>
2022-01-31 21:12:00 +0100

    accountant1 - klasy i metody, accountant - petla

---
+++ accountant/accountant1.py
@@ -0,0 +1,157 @@
+import sys
+historia = []
+magazyn = {}
+konto = 0
+
+
+def wczytaj_parametry(ilosc_lini, zrodlo, plik=None):
+    lista = []
+    status = True
+    if zrodlo == "plik":
+        for numer_lini in range(ilosc_lini):
+            linia = plik.readline()
+            if not linia:
+                return False, lista
+            linia = linia.strip()
+            lista.append(linia)
+        return True, lista
+    if zrodlo == "argv":
+        if len(sys.argv) >= ilosc_lini + 1:
+            return True, sys.argv[1:ilosc_lini+1]
+        else:
+            return False, sys.argv[1:]
+
+
+class Saldo:
+    def __init__(self):
+        self.kwota = 0
+        self.komentarz = ""
+
+    def dostep(self, zrodlo, plik=None):
+        status, lista = wczytaj_parametry(2, zrodlo, plik)
+        if not status:
+            print("Blad - niewlasciwe parametry dla salda")
+            return False
+#lista jest = ["1000","wplata"]
+        self.kwota = int(lista[0])
+        self.komentarz = lista[1]
+        return True
+
+    def dostep_argv(self, zrodlo, plik=None):
+        status, lista = wczytaj_parametry(2, zrodlo, plik)
+        if not status:
+            print("Blad - niewlasciwe parametry dla salda")
+            return False
+        # lista jest = ["1000","wplata"]
+        self.kwota = int(sys.argv[2])
+        self.komentarz = sys.argv[3]
+        # print("kwota", self.kwota)
+        return True
+
+    def wykonaj(self, konto, magazyn):
+        if self.kwota + konto < 0:
+            print("Blad")
+            return False, konto
+        konto += self.kwota
+        return True, konto
+
+    def zapisz(self,plik):
+        plik.write("saldo\n")
+        plik.write(f"{self.kwota}\n")
+        plik.write(f"{self.komentarz}\n")
+
+
+
+class Zakup:
+    def __init__(self):
+        self.nazwa = ""
+        self.cena = 0
+        self.ilosc = 0
+
+    def dostep(self, zrodlo, plik=None):
+        status, lista = wczytaj_parametry(3, zrodlo, plik)
+        if not status:
+            print("Blad - niewlasciwe parametry dla zakup")
+            return False
+        self.nazwa = (lista[0])
+        self.cena = int(lista[1])
+        self.ilosc = int(lista[2])
+        return True
+
+    def dostep_argv(self, zrodlo, plik=None):
+        status, lista = wczytaj_parametry(3, zrodlo, plik)
+        if not status:
+            print("Blad - niewlasciwe parametry dla zakup")
+            return False
+        self.nazwa = (sys.argv[2])
+        self.cena = int(sys.argv[3])
+        self.ilosc = int(sys.argv[4])
+        return True
+
+    def wykonaj(self, konto, magazyn):
+        if konto - (self.cena * self.ilosc) < 0:
+            print("Nie masz wystarczających środków finansowych.")
+            return False, konto, magazyn
+        else:
+            if self.nazwa not in magazyn:
+                magazyn[self.nazwa] = self.ilosc
+                konto -= self.cena * self.ilosc
+            else:
+                magazyn[self.nazwa] += self.ilosc
+                konto -= self.cena * self.ilosc
+            return True, konto, magazyn
+
+    def zapisz(self, plik):
+        plik.write("zakup\n")
+        plik.write(f"{self.nazwa}\n")
+        plik.write(f"{self.cena}\n")
+        plik.write(f"{self.ilosc}\n")
+
+
+class Sprzedaz:
+    def __init__(self):
+        self.nazwa = ""
+        self.cena = 0
+        self.ilosc = 0
+
+    def dostep(self, zrodlo, plik=None):
+        status, lista = wczytaj_parametry(3, zrodlo, plik)
+        if not status:
+            print("Blad - niewlasciwe parametry dla sprzedaz")
+            return False
+        self.nazwa = (lista[0])
+        self.cena = int(lista[1])
+        self.ilosc = int(lista[2])
+        return True
+
+    def dostep_argv(self, zrodlo, plik=None):
+        status, lista = wczytaj_parametry(3, zrodlo, plik)
+        if not status:
+            print("Blad - niewlasciwe parametry dla sprzedaz")
+            return False
+        self.nazwa = (sys.argv[2])
+        self.cena = int(sys.argv[3])
+        self.ilosc = int(sys.argv[4])
+        return True
+
+    def wykonaj(self, konto, magazyn):
+        if self.nazwa not in magazyn:
+            print("Nie ma na stanie magazynu.")
+            return False, konto, magazyn
+        else:
+            if magazyn[self.nazwa] - self.ilosc < 0:
+                print("Bląd - nie ma wystarczającej ilosci w magazynie")
+                return False, konto, magazyn, magazyn[self.nazwa]
+            else:
+                magazyn[self.nazwa] -= self.ilosc
+                konto += self.cena * self.ilosc
+            return True, konto, magazyn
+
+    def zapisz(self, plik):
+        plik.write("sprzedaz\n")
+        plik.write(f"{self.nazwa}\n")
+        plik.write(f"{self.cena}\n")
+        plik.write(f"{self.ilosc}\n")
+
+
+
---
+++ accountant/accountantOLD.py
@@ -0,0 +1,123 @@
+import sys
+
+historia = []
+magazyn = {}
+przeglad = []
+konto = 0
+
+while True:
+    dostep = input()
+    if dostep != "saldo" and dostep != "zakup" and dostep != "sprzedaz":
+        break
+    if dostep == "saldo":
+        kwota = int(input())
+        konto = konto + kwota
+        if konto < 0:
+            print("Brak srodkow")
+            break
+        komentarz = input()
+        historia.append("saldo")
+        historia.append(kwota)
+        historia.append(komentarz)
+        zdarzenie = "saldo", kwota, komentarz
+        przeglad.append(zdarzenie)
+    if dostep == "zakup":
+        nazwa = input()
+        cena = int(input())
+        ilosc = int(input())
+        if konto - (cena * ilosc) < 0:
+            print("Nie masz wystarczających środków finansowych."
+                  "Twoje saldo to:", konto)
+            continue
+        if nazwa in magazyn:
+            magazyn[nazwa] += ilosc
+        else:
+            magazyn[nazwa] = ilosc
+        konto -= cena * ilosc
+        historia.append("zakup")
+        historia.append(nazwa)
+        historia.append(cena)
+        historia.append(ilosc)
+        zdarzenie = "zakup", nazwa, cena, ilosc
+        przeglad.append(zdarzenie)
+    if dostep == "sprzedaz":
+        nazwa = input()
+        if nazwa not in magazyn:
+            print("Nie ma w magazynie")
+            continue
+        cena = int(input())
+        ilosc = int(input())
+        if nazwa in magazyn:
+            if magazyn[nazwa] - ilosc >= 0:
+                magazyn[nazwa] -= ilosc
+                konto += cena * ilosc
+            else:
+                print("za mała ilosc w magazynie")
+                continue
+        historia.append("sprzedaz")
+        historia.append(nazwa)
+        historia.append(cena)
+        historia.append(ilosc)
+        zdarzenie = "sprzedaz", nazwa, cena, ilosc
+        przeglad.append(zdarzenie)
+if len(sys.argv) > 1:
+    if sys.argv[1] == "historia":
+        for a in historia:
+            print(a)
+    if sys.argv[1] == "magazyn":
+        nazwa = sys.argv[2] and sys.argv[3] and sys.argv[4]
+        if nazwa not in magazyn:
+            magazyn[nazwa] = 0
+        if nazwa in magazyn:
+            for nazwa, ilosc in magazyn.items():
+                print("{}: {}".format(nazwa, ilosc))
+    if sys.argv[1] == "konto":
+        print(konto)
+    if sys.argv[1] == "przeglad":
+        od = int(sys.argv[2])
+        do = int(sys.argv[3])
+        for wiersz in przeglad[od:do+1]:
+            for element in wiersz:
+                print(element)
+    if sys.argv[1] == "zakup":
+        nazwa = sys.argv[2]
+        cena = int(sys.argv[3])
+        ilosc = int(sys.argv[4])
+        if konto - (cena * ilosc) < 0:
+            print("Nie masz wystarczających środków finansowych."
+                  "Twoje saldo to:", konto)
+        if nazwa in magazyn:
+            magazyn[nazwa] += ilosc
+        else:
+            magazyn[nazwa] = ilosc
+        konto -= cena * ilosc
+        historia.append("zakup")
+        historia.append(nazwa)
+        historia.append(cena)
+        historia.append(ilosc)
+        zdarzenie = "zakup", nazwa, cena, ilosc
+        przeglad.append(zdarzenie)
+    if sys.argv[1] == "sprzedaz":
+        nazwa = sys.argv[2]
+        if nazwa not in magazyn:
+            print("Nie ma takiego produktu")
+        else:
+            cena = int(sys.argv[3])
+            ilosc = int(sys.argv[4])
+            if nazwa in magazyn:
+                if magazyn[nazwa] - ilosc >= 0:
+                    magazyn[nazwa] -= ilosc
+                    konto += cena * ilosc
+                else:
+                    print("za mała ilosc w magazynie")
+            historia.append("sprzedaz")
+            historia.append(nazwa)
+            historia.append(cena)
+            historia.append(ilosc)
+            zdarzenie = "sprzedaz", nazwa, cena, ilosc
+            przeglad.append(zdarzenie)
+    if sys.argv[1] in ("zakup", "sprzedaz"):
+        for b in historia:
+            print(b)
+    if dostep == "stop":
+        print("stop")
