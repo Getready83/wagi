0ad70e420c5b74d9d02edb6ed82c7a363690c853
unknown <p.getler@o2.pl>
2022-04-22 00:06:10 +0200

    słownikiilisty

--- exercise/exercise4.py
+++ exercise/exercise4.py
@@ -12,3 +12,22 @@ for key in D:
 
 x = "myszydokazujągdykotanieczują"
 
+D = {}
+for litera in x:
+    if litera not in D.keys():
+        D[litera] = 1
+    else:
+        D[litera] += 1
+
+S = {x:x+1 for x in range(10000) if x%23 == 0}
+E = {a : a+2 for a in range(100) if a%5 == 0}
+
+litera = {'m': 1, 'y': 3, 's': 1, 'z': 3, 'd': 2, 'o': 2, 'k': 2, 'a': 2, 'u': 2, 'j': 2, 'ą': 2, 'g': 1, 't': 1, 'n': 1, 'i': 1, 'e': 1, 'c': 1}
+
+print(list(litera.values()))
+
+print(True if 4 in list(litera.values())else False)
+
+pensje = {'ksiegowa': 5000, 'kierowca': 4500, 'recepcjonistka': 4000}
+
+print(sum(list(pensje.values())))
