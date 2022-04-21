7dc58a9aa2ceedc22b7b31e2e4f4fab1680ccb6f
unknown <p.getler@o2.pl>
2022-04-21 03:09:45 +0200

    exercises

---
+++ exercise/exercise4.py
@@ -0,0 +1,14 @@
+#co zostanie wypisane w wywniku poniższego kodu
+
+D = {1:"Ala", 2: "ma", 3: "kota"}
+
+for key in D:
+    print(D[key])
+
+# w zależnosci od tego jaka wersja pythona
+
+# pyt dla danego stringa x stwórz słownik przechowujący informacje ile razy dana
+# litera występuje w stringu
+
+x = "myszydokazujągdykotanieczują"
+
